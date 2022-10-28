2f7e5dc2f95b8f1f1d2e1cc3bd42b8136134a55d
NectoT <nectot2@gmail.com>
2022-10-28 12:43:52 +0300

    manager.py now terminates its subprocesses when exited

--- manager.py
+++ manager.py
@@ -1,16 +1,28 @@
 import os
 import subprocess
 import time
+import atexit
 
 '''
-    Syncs the working script with origin
+    Syncs the working script with origin. 
     
     DON'T USE THIS FOR DEVELOPMENT, SINCE IT DELETES (or at least should) ALL CHANGES NOT SAVED IN ORIGIN
 '''
 
+
+process = None
+
+
+def on_exit():
+    if process is not None:
+        process.terminate()
+    print("Exiting")
+
+
+atexit.register(on_exit)
 while True:
     os.system("git pull origin")
     print("Pulled newest version from origin")
-    process = subprocess.Popen("python3 logic.py", shell=True)
+    process = subprocess.Popen("python3 logic.py", shell=False)
     time.sleep(5 * 60)
-    process.kill()
+    process.terminate()
