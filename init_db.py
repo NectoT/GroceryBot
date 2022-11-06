84fec36ee052d6d35c5d99942cd99901dbe7bd3d
NectoT <nectot2@gmail.com>
2022-11-06 19:55:49 +0300

    manager.py is now able to update the database

--- init_db.py
+++ init_db.py
@@ -1,23 +1,75 @@
 import psycopg2
+from psycopg2.errors import UndefinedTable
 from constants import DBNAME, DBPASSWORD
 
-connection = psycopg2.connect("dbname={0} user=postgres host='localhost' password={1}".format(DBNAME, DBPASSWORD))
-cursor = connection.cursor()
-cursor.execute(
-    '''CREATE TABLE groceryLists (
-        id integer PRIMARY KEY,
-        items text[] DEFAULT array[]::text[]
-    );
 
-    CREATE TABLE groceryUser (
-        id integer PRIMARY KEY,
-        grocery_list integer REFERENCES groceryLists (id) ON DELETE CASCADE
-    );'''
-)
+VALID_ID = 1  # increment if changes to db needs to be made
 
-for i in range(1000, 10000):
-    cursor.execute('INSERT INTO groceryLists (id) VALUES (%s);', [i])
 
-connection.commit()
-cursor.close()
-connection.close()
+def valid():
+    connection = psycopg2.connect("dbname={0} user=postgres host='localhost' password={1}".format(DBNAME, DBPASSWORD))
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT id from valid where id = %s", [VALID_ID])
+        cursor.close()
+        connection.close()
+
+        return cursor.rowcount != 1
+    except UndefinedTable:
+        cursor.close()
+        connection.close()
+        return False
+
+
+# Use this to tell manager what needs to be updated in the server database
+# (You can update your database manually just fine, this function exists for manager auto updating from GitHub)
+def update():
+    connection = psycopg2.connect("dbname={0} user=postgres host='localhost' password={1}".format(DBNAME, DBPASSWORD))
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE valid (
+            id integer PRIMARY KEY
+        );
+        
+        INSERT INTO valid VALUES (%s)
+    ''', [VALID_ID])
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "main":
+    connection = psycopg2.connect("dbname={0} user=postgres host='localhost' password={1}".format(DBNAME, DBPASSWORD))
+    cursor = connection.cursor()
+
+    # deleting old version if existed
+    cursor.execute('''
+        DROP TABLE IF EXISTS groceryLists
+        DROP TABLE IF EXISTS groceryUser
+        DROP TABLE IF EXISTS valid
+    ''')
+
+    cursor.execute(
+        '''CREATE TABLE groceryLists (
+            id integer PRIMARY KEY,
+            items text[] DEFAULT array[]::text[]
+        );
+    
+        CREATE TABLE groceryUser (
+            id integer PRIMARY KEY,
+            grocery_list integer REFERENCES groceryLists (id) ON DELETE CASCADE
+        );
+        
+        CREATE TABLE valid {
+            id integer PRIMARY KEY
+        };
+        
+        INSERT INTO valid VALUES (%s)''', [VALID_ID]
+    )
+
+    for i in range(1000, 10000):
+        cursor.execute('INSERT INTO groceryLists (id) VALUES (%s);', [i])
+
+    connection.commit()
+    cursor.close()
+    connection.close()
--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import time
 import atexit
+import init_db
 
 '''
     Syncs the working script with origin. 
@@ -23,6 +24,10 @@ atexit.register(on_exit)
 while True:
     os.system("git pull origin")
     print("Pulled newest version from origin")
+    if not init_db.valid():
+        print("Database update required")
+        init_db.update()
+        print("Database updated")
     process = subprocess.Popen(["python3", "logic.py"], shell=False)
     time.sleep(5 * 60)
     process.terminate()
